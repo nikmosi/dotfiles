b0cb56006f67c52fb5539cf0eb8385a31c66a3f8
nikmosi <i@nikmosi.ru>
2024-07-15 00:17:02 +0700

    remove useless

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,20 +27,11 @@ if status is-interactive
 
     alias x='extract'
 
-    abbr - 'cd -'
-    alias ...=../..
-    alias ....=../../..
-    alias .....=../../../..
-    alias ......=../../../../..
-    alias 1='cd -1'
-    alias 2='cd -2'
-    alias 3='cd -3'
-    alias 4='cd -4'
-    alias 5='cd -5'
-    alias 6='cd -6'
-    alias 7='cd -7'
-    alias 8='cd -8'
-    alias 9='cd -9'
+    abbr - 'z -'
+    abbr --position anywhere -a ... ../..
+    abbr --position anywhere -a .... ../../..
+    abbr --position anywhere -a ..... ../../../..
+    abbr --position anywhere -a ...... ../../../../..
 
     abbr -a CA --position anywhere '2>&1 | cat -A'
     abbr -a G --position anywhere '| rg'
