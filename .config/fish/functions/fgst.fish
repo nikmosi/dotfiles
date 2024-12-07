74252891fbd9f3e384c3c0f9bfc57cf284f492fd
nikmosi <i@nikmosi.ru>
2024-12-07 23:47:49 +0700

    fix

--- .config/fish/functions/fgst.fish
+++ .config/fish/functions/fgst.fish
@@ -7,14 +7,11 @@ function fgst
     end
 
     # Use fzf to select files
-    set selected (git -c color.status=always status --short | \
-        fzf --height 50% $argv --border -m --ansi --nth 2..,.. \
-        --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' | \
-        cut -c4- | sed 's/.* -> //')
+    set selected (git -c color.status=always status --short | fzf --height 50% $argv --border -m --ansi --nth 2..,.. --preview 'git diff --color=always -- {-1} | sed 1,4d; cat {-1}' | cut -c4- | sed 's/.* -> //')
 
     # Open selected files in editor
     if test -n "$selected"
-        for prog in (echo $selected)
+        for prog in $selected
             $EDITOR $prog
         end
     end
